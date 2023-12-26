cebdccf2603d4f71ed2248b04eb0aa25dcd2b9fb
amdryzensd <amdryzensd@gmail.com>
2023-12-26 18:24:46 +0530

    token persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { UserContext } from './context/userContext';
 import Router from './routes';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState("")
+
+  useEffect(()=>{
+    setIsAuthenticated(localStorage.getItem("token") || "")
+  },[])
+
   return (
     <UserContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
       <Router />
